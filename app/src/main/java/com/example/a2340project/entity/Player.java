b5938a617ab0c206297bd19ad5261adea8be03f2
haoranx3 <haoranx3@vt.edu>
2019-06-15 15:54:44 -0400

    Update Player class

--- app/src/main/java/com/example/a2340project/entity/Player.java
+++ app/src/main/java/com/example/a2340project/entity/Player.java
@@ -12,7 +12,9 @@ public class Player {
     private int fighter;
     private int trader;
     private int engineering;
+
     private static List<String> diffList = Arrays.asList("Beginners", "Easy", "Normal", "Hard", "Impossible");
+
     public Player() {
         this("default_player", "Beginner", 4, 4, 4, 4);
     }
@@ -25,6 +27,51 @@ public class Player {
         trader = t;
         engineering = e;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getEngineering() {
+        return engineering;
+    }
+
+    public void setEngineering(int engineering) {
+        this.engineering = engineering;
+    }
+
+    public int getPilot() {
+        return pilot;
+    }
+
+    public void setPilot(int p) {
+        pilot = p;
+    }
+
+    public int getFighter() {
+        return fighter;
+    }
+
+    public void setFighter(int f) {
+        fighter = p;
+    }
+
+    public int getTrader() {
+        return trader;
+    }
+
+    public String getDifficulties() {
+        return difficulties;
+    }
+
+    public void setDifficulties(String difficulties) {
+        this.difficulties = difficulties;
+    }
+
 
 
 }
