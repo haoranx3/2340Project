a36e9fedb97132a5379c08ef021b3aa6eb4e00a6
anthony <anthonyzhengscholar@gmail.com>
2019-06-16 06:14:58 -0400

    Completely revamped UI and revised event handling code

--- app/src/main/java/com/example/a2340project/views/MainActivity.java
+++ app/src/main/java/com/example/a2340project/views/MainActivity.java
@@ -23,35 +23,9 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                Intent createPlayerScreenIntent = new Intent(getApplicationContext(), createPlayerActivity.class);
+                startActivity(createPlayerScreenIntent);
             }
         });
-    }
-
-    public void openActivity2() {
-        Intent intent = new Intent(this, createPlayerActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/a2340project/views/createPlayerActivity.java
+++ app/src/main/java/com/example/a2340project/views/createPlayerActivity.java
@@ -1,33 +1,48 @@
 package com.example.a2340project.views;
 
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.a2340project.R;
 import com.example.a2340project.entity.Player;
 import com.example.a2340project.viewmodels.CreatePlayerViewModel;
+
+import com.example.a2340project.R;
 
 public class createPlayerActivity extends AppCompatActivity {
 
     private CreatePlayerViewModel viewModel;
 
-    private TextView idField;
+    private TextView skillPoints;
+    private TextView skillPointsShadow;
+
     private EditText usernameField;
-    private EditText pilotField;
-    private EditText fighterField;
-    private EditText traderField;
-    private EditText engineerField;
-    private Spinner diffSpinner;
+
+    private TextView sailorPoints;
+    private Button sailorMinus;
+    private Button sailorPlus;
+
+    private TextView trainerPoints;
+    private Button trainerMinus;
+    private Button trainerPlus;
+
+    private TextView traderPoints;
+    private Button traderMinus;
+    private Button traderPlus;
+
+    private TextView engineerPoints;
+    private Button engineerMinus;
+    private Button engineerPlus;
+
+    private Button createButton;
     private Player player;
-    private boolean editing;
+
+    private boolean noPointsLeft;
+    private int numPoints;
 
 
     @Override
@@ -35,60 +50,217 @@ public class createPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_player);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setHomeButtonEnabled(false);
-        }
-        idField = findViewById(R.id.Player_id_field);
-        usernameField = findViewById(R.id.player_name_input);
-        pilotField = findViewById(R.id.PilotSkill_spinner);
-        fighterField = findViewById(R.id.FighterSkill_spinner);
-        traderField = findViewById(R.id.TraderSkill_spinner);
-        engineerField = findViewById(R.id.EngineerSkill_spinner);
-        diffSpinner = findViewById(R.id.Difficulty_spinner);
+        skillPoints = findViewById(R.id.pointsText);
+        skillPointsShadow = findViewById(R.id.pointsTextShadow);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Player.diffList);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        diffSpinner.setAdapter(adapter);
+        usernameField = findViewById(R.id.usernameInput);
 
-        player = new Player();
-        usernameField.setText(player.getUserName());
-        idField.setText(String.format("Player ID: %d", player.getId()));
-        pilotField.setText(player.getPilot()+"");
-        fighterField.setText(player.getFighter()+"");
-        traderField.setText(player.getTrader()+"");
-        engineerField.setText(player.getEngineering()+"");
-        viewModel = ViewModelProviders.of(this).get(CreatePlayerViewModel.class);
-    }
+        sailorPoints = findViewById(R.id.sailorPoints);
+        sailorMinus = findViewById(R.id.sailorMinus);
+        sailorPlus = findViewById(R.id.sailorPlus);
 
-    public void onAddPressed(View view) {
-        player.setUserName(usernameField.getText().toString());
-        player.setDifficulties((String) diffSpinner.getSelectedItem());
-        int pilot = Integer.valueOf(pilotField.getText().toString());
-        int trade = Integer.valueOf(traderField.getText().toString());
-        int engineer = Integer.valueOf(engineerField.getText().toString());
-        int fighter = Integer.valueOf(fighterField.getText().toString());
-        if (pilot + trade + engineer + fighter != 16) {
-            Context context = getApplicationContext();
-            CharSequence text = "Skill Points must sum to 16!";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        } else {
-            player.setPilot(pilot);
-            player.setFighter(fighter);
-            player.setEngineering(engineer);
-            player.setTrader(trade);
-            viewModel.addPlayer(player);
-            Context context = getApplicationContext();
-            CharSequence text = "Player # " + player.getId() + " created! ";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-    }
+        trainerPoints = findViewById(R.id.trainerPoints);
+        trainerMinus = findViewById(R.id.trainerMinus);
+        trainerPlus = findViewById(R.id.trainerPlus);
 
-    public void onCancelPressed(View view) {
-        onBackPressed();
+        traderPoints = findViewById(R.id.traderPoints);
+        traderMinus = findViewById(R.id.traderMinus);
+        traderPlus = findViewById(R.id.traderPlus);
+
+        engineerPoints = findViewById(R.id.engineerPoints);
+        engineerMinus = findViewById(R.id.engineerMinus);
+        engineerPlus = findViewById(R.id.engineerPlus);
+
+        createButton = findViewById(R.id.createButton);
+
+        noPointsLeft = false;
+        numPoints = 16;
+
+        sailorMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(sailorPoints.getText().toString());
+                if (numPoints != 16 && pts != 0) {
+                    numPoints++;
+                    String totalPoints = numPoints + "pts";
+                    sailorPoints.setText(String.valueOf(pts - 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        sailorPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(sailorPoints.getText().toString());
+                if (numPoints != 0 && pts != 16) {
+                    numPoints--;
+                    String totalPoints = numPoints + "pts";
+                    sailorPoints.setText(String.valueOf(pts + 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        trainerMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(trainerPoints.getText().toString());
+                if (numPoints != 16 && pts != 0) {
+                    numPoints++;
+                    String totalPoints = numPoints + "pts";
+                    trainerPoints.setText(String.valueOf(pts - 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        trainerPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(trainerPoints.getText().toString());
+                if (numPoints != 0 && pts != 16) {
+                    numPoints--;
+                    String totalPoints = numPoints + "pts";
+                    trainerPoints.setText(String.valueOf(pts + 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        traderMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(traderPoints.getText().toString());
+                if (numPoints != 16 && pts != 0) {
+                    numPoints++;
+                    String totalPoints = numPoints + "pts";
+                    traderPoints.setText(String.valueOf(pts - 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        traderPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(traderPoints.getText().toString());
+                if (numPoints != 0 && pts != 16) {
+                    numPoints--;
+                    String totalPoints = numPoints + "pts";
+                    traderPoints.setText(String.valueOf(pts + 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        engineerMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(engineerPoints.getText().toString());
+                if (numPoints != 16 && pts != 0) {
+                    numPoints++;
+                    String totalPoints = numPoints + "pts";
+                    engineerPoints.setText(String.valueOf(pts - 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        engineerPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pts = Integer.parseInt(engineerPoints.getText().toString());
+                if (numPoints != 0 && pts != 16) {
+                    numPoints--;
+                    String totalPoints = numPoints + "pts";
+                    engineerPoints.setText(String.valueOf(pts + 1));
+                    skillPoints.setText(totalPoints);
+                    skillPointsShadow.setText(totalPoints);
+                }
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (usernameField.getText().toString() == "" || numPoints != 0) {
+                    Toast.makeText(getApplicationContext(), "Can't do that!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "To be implemented later", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
+
+
+
+
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//            getSupportActionBar().setHomeButtonEnabled(false);
+//        }
+//        idField = findViewById(R.id.Player_id_field);
+//        usernameField = findViewById(R.id.player_name_input);
+//        pilotField = findViewById(R.id.PilotSkill_spinner);
+//        fighterField = findViewById(R.id.FighterSkill_spinner);
+//        traderField = findViewById(R.id.TraderSkill_spinner);
+//        engineerField = findViewById(R.id.EngineerSkill_spinner);
+//        diffSpinner = findViewById(R.id.Difficulty_spinner);
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Player.diffList);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        diffSpinner.setAdapter(adapter);
+//
+//        player = new Player();
+//        usernameField.setText(player.getUserName());
+//        idField.setText(String.format("Player ID: %d", player.getId()));
+//        pilotField.setText(player.getPilot()+"");
+//        fighterField.setText(player.getFighter()+"");
+//        traderField.setText(player.getTrader()+"");
+//        engineerField.setText(player.getEngineering()+"");
+//        viewModel = ViewModelProviders.of(this).get(CreatePlayerViewModel.class);
+
+
+
+
+//
+//    public void onAddPressed(View view) {
+//        player.setUserName(usernameField.getText().toString());
+//        player.setDifficulties((String) diffSpinner.getSelectedItem());
+//        int pilot = Integer.valueOf(pilotField.getText().toString());
+//        int trade = Integer.valueOf(traderField.getText().toString());
+//        int engineer = Integer.valueOf(engineerField.getText().toString());
+//        int fighter = Integer.valueOf(fighterField.getText().toString());
+//        if (pilot + trade + engineer + fighter != 16) {
+//            Context context = getApplicationContext();
+//            CharSequence text = "Skill Points must sum to 16!";
+//            int duration = Toast.LENGTH_SHORT;
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        } else {
+//            player.setPilot(pilot);
+//            player.setFighter(fighter);
+//            player.setEngineering(engineer);
+//            player.setTrader(trade);
+//            viewModel.addPlayer(player);
+//            Context context = getApplicationContext();
+//            CharSequence text = "Player # " + player.getId() + " created! ";
+//            int duration = Toast.LENGTH_SHORT;
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        }
+//    }
+//
+//    public void onCancelPressed(View view) {
+//        onBackPressed();
+//    }
