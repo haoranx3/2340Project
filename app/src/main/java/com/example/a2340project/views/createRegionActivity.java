5843bd4d64ada92092650ec410d375d88539e5be
haoranx3 <haoranx3@vt.edu>
2019-07-13 13:53:30 -0400

    added menu

--- app/src/main/java/com/example/a2340project/views/createRegionActivity.java
+++ app/src/main/java/com/example/a2340project/views/createRegionActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -1051,6 +1053,13 @@ public class createRegionActivity extends AppCompatActivity {
                 }
             }
         }, 100);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 }
